6879b6154da6d96cdfad9ecd8c42b13405831334
Victor Melançon <victormelancon@MacBook-Pro-de-Victor.local>
2022-02-23 14:47:46 +0100

    real oopsie

--- SuperAlbert/model.py
+++ SuperAlbert/model.py
@@ -9,9 +9,21 @@ import torchvision.models as models
 
 # from resnet_model import create_pretrained_resnet
 
-def create_model(output_size):
+def create_model_b4(output_size):
     # Loading pretrained model
     model = models.efficientnet_b4(pretrained=True)
+    # setting all parameters as constants
+    for p in model.parameters():
+        p.requires_grad = False
+
+    # changing the last layer for a newly initialized trainable classifier
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, output_size)
+
+    return model
+
+def create_model_b3(output_size):
+    # Loading pretrained model
+    model = models.efficientnet_b3(pretrained=True)
     # setting all parameters as constants
     for p in model.parameters():
         p.requires_grad = False
